f742b29e5fbae10a07de909d9c7f9f028e6a3df3
hasanhayat <aylskhsmvz.v@gmail.com>
2024-12-06 08:20:00 +0500

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Card from './components/card'
 
 function App() {
   return (
@@ -106,6 +107,7 @@ function App() {
               <div className="send"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"/></svg><p>Send</p></div>
             </div>
           </div>
+          <Card profileimg="https://scontent.fisb5-1.fna.fbcdn.net/v/t39.30808-1/339733955_236683938826968_6836318394336583613_n.jpg?stp=cp0_dst-jpg_s40x40&_nc_cat=1&ccb=1-7&_nc_sid=f4b9fd&_nc_eui2=AeGKSIRmY_YbjLWPFqojuAXKcs6VTrNht55yzpVOs2G3nuEfBYLWovegcpAV0QYJp_RHCaXEar9i3yfhcmZp6FBO&_nc_ohc=5BUajwizpFUQ7kNvgHbR9Ey&_nc_zt=24&_nc_ht=scontent.fisb5-1.fna&_nc_gid=A9rhVgO6WsRAvdTl2Lr2VV3&oh=00_AYDhFtsnUruYndxHNQNgvHPrxWkrrjTIIf5gjitnKQdaKw&oe=6758339A" heading="It's official! COLOR of the year! 🤎" time=">4 December at 12:36" img="https://scontent.fisb5-1.fna.fbcdn.net/v/t39.30808-6/468621694_122170871180108483_3896420752229502124_n.jpg?stp=dst-jpg_p552x414&_nc_cat=1&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeG4N6WRNkmgRVdTglpsIIZAaSfxUK8ccNppJ_FQrxxw2t0PBpiKMAJTuoDNQDqtbfpCMxvCgv5JegTRLCOxtftK&_nc_ohc=uM2X4JK9-5QQ7kNvgGcCeiq&_nc_zt=23&_nc_ht=scontent.fisb5-1.fna&_nc_gid=AbX6GLMTtMmuh_xZOT_xsjN&oh=00_AYCdoY1wLYMErpsBb0F5n0GtpPMZ0yfhZtS7ZvbDgamXlg&oe=67582D76"></Card>
         </section>
         <section className="sec2"></section>
       </main>
